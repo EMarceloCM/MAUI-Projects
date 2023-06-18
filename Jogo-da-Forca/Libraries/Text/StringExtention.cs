d11ded752bc9854773a7fd7ee9d115b40684b341
Marcelo <120042864+EMarceloCM@users.noreply.github.com>
2023-06-18 15:02:03 -0300

    Add files via upload

---
+++ Jogo-da-Forca/Libraries/Text/StringExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_Forca.Libraries.Text
+{
+    static class StringExtention
+    {
+        public static List<int> AllIndexesOff(this string str, string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentNullException("A palavra nao pode ser vazia!", "value");
+            List<int> indexes = new List<int>();
+            for (int i = 0; ; i += value.Length)
+            {
+                i = str.IndexOf(value, i);
+                if (i == -1)
+                    return indexes;
+                indexes.Add(i);
+            }
+        }
+    }
+}
