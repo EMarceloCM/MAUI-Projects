4809fbc07ce07999659732dc4b591f6a7d776e38
EMarceloCM <edmylsonmarcelo3@gmail.com>
2023-06-30 10:33:04 -0300

    new file

---
+++ Task/AppTask.Models/SubTaskModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace AppTask.Models
+{
+    public class SubTaskModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        
+        private bool _IsCompleted;
+        public bool IsCompleted
+        {
+            get { return _IsCompleted; }
+            set { _IsCompleted = value; OnPropertyChanged(nameof(IsCompleted)); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Task/AppTask.Models/TaskModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace AppTask.Models
+{
+    public class TaskModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public DateTime PrevisionDate { get; set;}
+        
+        private bool _IsCompleted;
+        public bool IsCompleted { 
+            get { return _IsCompleted; } 
+            set { _IsCompleted = value; OnPropertyChanged(nameof(IsCompleted)); } 
+        }
+        public DateTime Created { get; set;}
+        public DateTime Updated { get; set;}
+        public ObservableCollection<SubTaskModel> SubTasks { get; set;} = new ObservableCollection<SubTaskModel>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
