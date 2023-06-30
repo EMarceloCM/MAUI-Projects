c95a0715391f2e320663cd0e444f1c7770660115
EMarceloCM <edmylsonmarcelo3@gmail.com>
2023-06-30 10:06:16 -0300

    'asd'

---
+++ source/repos/AppTask.DataBase/AppTaskContext.cs
@@ -0,0 +1,28 @@
+﻿using AppTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppTask.DataBase
+{
+    public class AppTaskContext : DbContext
+    {
+        //Tabelas
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<SubTaskModel> SubTasks { get; set; }
+
+        public AppTaskContext()
+        {
+            //Cria o arquivo se nao existir, verifica se possui migrações caso exista
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dataBasePath = Path.Combine(folderPath, "apptask.db");
+
+            optionsBuilder.UseSqlite($"FileName={dataBasePath}");
+        }
+    }
+}
